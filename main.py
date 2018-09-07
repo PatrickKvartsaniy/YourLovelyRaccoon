06db2c7507f8e64cd78affbe04ded36430a9946e
PatrickKvartsaniy <kvartsaniy@gmail.com>
2018-09-07 12:39:55 +0300

    change url

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     try:
         app = loop.run_until_complete(init_app(loop))
         raccoon_bot = Raccoon(app['config']['telegram']['token'], loop)
-        app.router.add_route('POST', '/api/v1', raccoon_bot.handler)
+        app.router.add_route('POST', '/api/v1/echo', raccoon_bot.handler)
 
         app.middlewares.append(middleware_factory)
         web.run_app(app,host='0.0.0.0',port=23456)
