cc67e1d1c54181fee212a60d389f354a576a7d33
PatrickKvartsaniy <kvartsaniy@gmail.com>
2018-09-07 12:47:17 +0300

    change black list

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,6 +1,7 @@
 async def middleware_factory(app, handler):
     async def middleware_handler(request):
         data = await request.json()
+        black_list = []
         if data['message']['from']['id'] in black_list:
             return web.Response(status=200)
         return await handler(request)
