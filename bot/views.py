f12a7f3a2651a26641dbde63a7716a2e1b14114f
PatrickKvartsaniy <kvartsaniy@gmail.com>
2018-09-07 14:54:48 +0300

    add some basic dialog

--- bot/views.py
+++ bot/views.py
@@ -8,7 +8,9 @@ class Raccoon(Conversation):
         super().__init__(token, loop)
 
     async def _handler (self, message):
-        if "/events" in message['text']:
+        if "events" in message['text']:
             data = await self.getData("FutureEvents")
-            message_text = f"Зараз в нас плануються такі івенти: /n {data[0]['title']}, посилання - {data[0]['link']}"
-            await self.sendMessage(message['chat']['id'], message_text)
+            # message_text = f"Зараз в нас плануються такі івенти: /n {data[0]['title']}, посилання - {data[0]['link']}"
+            # await self.sendMessage(message['chat']['id'], message_text)
+            print(data)
+            print(data[0])
